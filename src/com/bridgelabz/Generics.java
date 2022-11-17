5d7e10d636b76cbd5400cc00d08e72b113c96aa0
Rohit <Rohit@spotblhol0128.awsspoton.com>
2022-11-17 17:29:55 +0530

    Added: UC1 maximum number from Interger

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,7 +1,25 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class Generics {
     public static void main(String[] args) {
-        System.out.println("welcome to generics program");
+        System.out.println("Welcome to the Generics Program");
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a value :: ");
+        a = sc.nextInt();
+        System.out.print("Enter b value :: ");
+        b = sc.nextInt();
+        System.out.print("Enter c value :: ");
+        c = sc.nextInt();
+
+        if (a > b && a > c){
+            System.out.println(a+" is bigger");
+        } else if (b > a && b > c) {
+            System.out.println(b+ " is bigger");
+        }else {
+            System.out.println(c+ " is bigger");
+        }
     }
 }
